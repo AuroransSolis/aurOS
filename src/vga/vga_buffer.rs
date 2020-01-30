8a5f719c98e8f327749895d8ccd76438acbbbc3c
Aurorans Solis <primalucegd@gmail.com>
2020-01-30 12:50:27 -0700

    Fleshed out helper functions and writing functions. Also made the transition from '&mut VgaBuffer' to '*mut VgaBuffer' so that I can avoid a 'lazy_static' dependency. Not using that, however, caused some problems that ended up forcing me to go from a mutable reference to a mutable pointer.

--- src/vga/vga_buffer.rs
+++ src/vga/vga_buffer.rs
@@ -1,5 +1,5 @@
+use super::vga_char::{VgaChar, VgaColour, BLANK_CHAR};
 use core::fmt;
-use super::vga_char::{VgaChar, VgaColour};
 use volatile::Volatile;
 
 const BUFFER_WIDTH: usize = 80;
@@ -13,18 +13,20 @@ struct VgaBuffer {
 pub struct VgaWriter {
     column_position: usize,
     colour_code: u8,
-    buf: &'static mut VgaBuffer,
+    // Using a pointer instead of a mutable reference since it makes sense to use one for memory
+    // that's out of our control
+    buf: *mut VgaBuffer,
 }
 
 impl VgaWriter {
     pub fn write_byte(&mut self, b: u8) {
         match b {
-            b'\n' => self.write_line(),
+            b'\n' => self.new_line(),
             _ => {
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
                 let colour_code = self.colour_code;
-                self.buf.chars[row][col].write(VgaChar::new(b, colour_code));
+                unsafe { &mut *self.buf }.chars[row][col].write(VgaChar::new(b, colour_code));
             }
         }
     }
@@ -35,46 +37,69 @@ impl VgaWriter {
                 // 0x20(space)..0x7e(pipe) or newline
                 b' '..=b'|' | b'\n' => self.write_byte(b),
                 // Everything else is outside the printable ASCII range, write ■ as a filler
-                _ => self.write_byte(0xfe)
+                _ => self.write_byte(0xfe),
             }
             self.advance_cursor();
         }
     }
 
-    pub fn write_line(&mut self) {
-
+    pub fn new_line(&mut self) {
+        for r in 1..BUFFER_HEIGHT {
+            for c in 0..BUFFER_WIDTH {
+                let copy = unsafe { &*self.buf }.chars[r][c].read();
+                unsafe { &mut *self.buf }.chars[r - c][c].write(copy);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
     }
 
-    pub fn advance_column(&mut self) {
-        self.column_position += 1;
+    pub fn clear_row(&mut self, row: usize) {
+        for c in 0..BUFFER_WIDTH {
+            unsafe { &mut *self.buf }.chars[row][c].write(BLANK_CHAR);
+        }
     }
 
-    pub fn set_colour_code(&mut self, fg: VgaColour, bg: VgaColour) {
+    pub fn set_fg_bg(&mut self, fg: VgaColour, bg: VgaColour) {
         self.colour_code = VgaColour::packed_fg_bg(fg, bg);
+    }
+
+    pub fn set_fg(&mut self, fg: VgaColour) {
+        // Set the lower four bits (foreground bits) to 0
+        self.colour_code &= 0b11110000;
+        // Apply the new foreground code
+        self.colour_code |= fg as u8;
+    }
+
+    pub fn set_bg(&mut self, bg: VgaColour) {
+        // Set the upper four bits (background bits) to 0
+        self.colour_code &= 0b00001111;
+        // Apply the new background code
+        self.colour_code |= (bg as u8) << 4;
     }
 
     pub fn advance_cursor(&mut self) {
         self.column_position += 1;
         if self.column_position == BUFFER_WIDTH {
             self.column_position = 0;
-            self.write_line();
+            self.new_line();
         }
     }
 }
+
+// Technically safe since only one VGA writer is constructed, and it should have unique access to
+// the VGA buffer pointer. Necessary for use in `spin::Mutex`.
+unsafe impl Send for VgaWriter {}
+
+// Global VGA writer - `spin::Mutex` is used for synchronized interior mutability.
+pub static VGA_WRITER: spin::Mutex<VgaWriter> = spin::Mutex::new(VgaWriter {
+    column_position: 0,
+    colour_code: VgaColour::packed_fg_bg(VgaColour::Red, VgaColour::White),
+    buf: 0xb8000 as *mut VgaBuffer,
+});
 
 impl fmt::Write for VgaWriter {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         Ok(self.write_string(s))
     }
-}
-
-pub fn vga_test() {
-    let mut writer = VgaWriter {
-        column_position: 0,
-        colour_code: VgaColour::packed_fg_bg(VgaColour::Red, VgaColour::White),
-        buf: unsafe { &mut *(0xb8000 as *mut VgaBuffer) }
-    };
-    writer.write_string("Hello");
-    writer.set_colour_code(VgaColour::White, VgaColour::Red);
-    writer.write_string(" world!");
 }
