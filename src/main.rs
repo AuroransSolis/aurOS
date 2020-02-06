5f09e0ef48676d9d7dbd65d67697f07e68c84878
Aurorans Solis <primalucegd@gmail.com>
2020-02-06 12:50:56 -0700

    Tried adding some testing stuff. Having issues with: 'signal: 11, SIGSEGV: invalid memory reference'.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #![no_std]
 #![no_main]
+// Required since the test derive macro is only part of std, so we need to make our own testing
+// framework.
+#![feature(custom_test_frameworks)]
+// Specify the path to our test runner.
+#![test_runner(crate::test_runner::test_runner)]
+// Testing generates a `main`, which is ignored because of the `!#[no_main]` above. To avoid this,
+// we can change the name of the function that gets created in a testing context.
+#![reexport_test_harness_main = "test_main"]
 
 #[macro_use]
 mod vga;
 mod panic;
+mod test_runner;
 
 use vga::{
     vga_buffer::{global_vga_bg, global_vga_fgbg, VGA_WRITER},
@@ -21,8 +30,18 @@ pub extern "C" fn _start() -> ! {
     println!("world!");
     global_vga_bg(VgaColour::Black);
     println!("Testing, testing, 123.");
+    // Run `test_main()` in the case that we're testing things.
+    #[cfg(test)]
+    test_main();
     // Check panic handler
     panic!("uh oh");
     // Returns `!` so why not lmao
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
