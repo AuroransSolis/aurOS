d1d6bbdaab1a0cfb34b849efcf280b61e2a81b58
Aurorans Solis <primalucegd@gmail.com>
2020-02-05 20:09:53 -0700

    Print out panic info on panic. Should be useful.

--- src/panic.rs
+++ src/panic.rs
@@ -3,6 +3,7 @@ use core::panic::PanicInfo;
 // Basic panic handler for now
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", info);
     // See notes for `_start`
     loop {}
 }
